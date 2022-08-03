8a825de7b396fb7f5ac7f7a742e55e71d20c9b17
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2022-08-03 14:18:51 +0700

    feat: add handleSubmit method

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Panel, Form, Input, DatePicker, ButtonToolbar, Button } from "rsuite";
 
-const Textarea = React.forwardRef((props, ref) => (
-  <Input {...props} as="textarea" ref={ref} />
+const Textarea = React.forwardRef((props, ref: any) => (
+  <Input {...props} as="textarea" rows={3} ref={ref} />
 ));
 
 export default function InputForm() {
+  const [formValue, setFormValue] = useState({
+    title: "",
+    deadline: "",
+    description: "",
+  });
+  const formRef = useRef<any>(null);
+
+  const handleSubmit = (status: boolean) => {
+    console.log({ formValue, status });
+  };
+
   return (
     <Panel header="Add Todo" bordered>
-      <Form fluid>
+      <Form
+        fluid
+        ref={formRef}
+        onSubmit={handleSubmit}
+        formValue={formValue}
+        onChange={(value: any) => setFormValue(value)}
+      >
         <Form.Group controlId="title">
           <Form.ControlLabel>Title</Form.ControlLabel>
           <Form.Control name="title" />
@@ -16,16 +33,27 @@ export default function InputForm() {
         </Form.Group>
         <Form.Group controlId="deadline">
           <Form.ControlLabel>Deadline Date</Form.ControlLabel>
-          <DatePicker block />
+          <DatePicker
+            oneTap
+            block
+            name="deadline"
+            onSelect={(date: Date) =>
+              setFormValue({ ...formValue, deadline: date.toLocaleString() })
+            }
+          />
         </Form.Group>
         <Form.Group controlId="description">
           <Form.ControlLabel>Description</Form.ControlLabel>
-          <Form.Control name="description" rows={3} accepter={Textarea} />
+          <Form.Control name="description" accepter={Textarea} />
         </Form.Group>
         <Form.Group>
           <ButtonToolbar>
-            <Button appearance="primary">Submit</Button>
-            <Button appearance="default">Cancel</Button>
+            <Button appearance="primary" type="submit">
+              Submit
+            </Button>
+            <Button appearance="default" type="reset">
+              Cancel
+            </Button>
           </ButtonToolbar>
         </Form.Group>
       </Form>
