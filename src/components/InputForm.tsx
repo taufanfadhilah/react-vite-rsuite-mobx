3064cb54f2f0f7a4e28bbc0ff03603ee274b1ea9
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2022-08-04 09:51:03 +0700

    feat: adding message

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from "react";
 import { TodoInputInterface } from "../types/Todo";
 import {
+  Animation,
   Panel,
   Form,
   Schema,
@@ -8,6 +9,7 @@ import {
   DatePicker,
   ButtonToolbar,
   Button,
+  Message,
 } from "rsuite";
 import { todoStore } from "../store";
 
@@ -16,6 +18,7 @@ const Textarea = React.forwardRef((props, ref: any) => (
 ));
 
 export default function InputForm() {
+  const [openMessage, setOpenMessage] = useState(false);
   const [formValue, setFormValue] = useState({
     title: "",
     deadline: new Date(),
@@ -31,52 +34,67 @@ export default function InputForm() {
     ),
   });
 
-  const handleSubmit = (status: boolean) => {
+  const handleSubmit = (status: boolean): void => {
     if (status) {
       todoStore.addTodo(formValue as TodoInputInterface);
+      setOpenMessage(true);
       handleReset();
     }
   };
 
-  const handleReset = () => {
+  const handleReset = (): void => {
     setFormValue({ title: "", deadline: new Date(), description: "" });
   };
 
   return (
-    <Panel header="Add Todo" bordered>
-      <Form
-        fluid
-        ref={formRef}
-        onSubmit={handleSubmit}
-        formValue={formValue}
-        onChange={(value: any) => setFormValue(value)}
-        onReset={handleReset}
-        model={model}
-      >
-        <Form.Group controlId="title">
-          <Form.ControlLabel>Title</Form.ControlLabel>
-          <Form.Control name="title" />
-          <Form.HelpText>Required</Form.HelpText>
-        </Form.Group>
-        <Form.Group controlId="deadline">
-          <Form.ControlLabel>Deadline Date</Form.ControlLabel>
-          <Form.Control name="deadline" accepter={DatePicker} oneTap block />
-        </Form.Group>
-        <Form.Group controlId="description">
-          <Form.ControlLabel>Description</Form.ControlLabel>
-          <Form.Control name="description" accepter={Textarea} />
-        </Form.Group>
-        <Form.Group>
-          <ButtonToolbar>
-            <Button appearance="primary" type="submit">
-              Submit
-            </Button>
-            <Button appearance="default" type="reset">
-              Cancel
-            </Button>
-          </ButtonToolbar>
-        </Form.Group>
-      </Form>
-    </Panel>
+    <>
+      <Animation.Collapse in={openMessage}>
+        <Message
+          showIcon
+          closable
+          onClose={() => setOpenMessage(false)}
+          type="success"
+          header="Success"
+          className="mb-4"
+        >
+          Successfully added a new todo.
+        </Message>
+      </Animation.Collapse>
+      <Panel header="Add Todo" bordered>
+        <Form
+          fluid
+          ref={formRef}
+          onSubmit={handleSubmit}
+          formValue={formValue}
+          onChange={(value: any) => setFormValue(value)}
+          onReset={handleReset}
+          model={model}
+        >
+          <Form.Group controlId="title">
+            <Form.ControlLabel>Title</Form.ControlLabel>
+            <Form.Control name="title" />
+            <Form.HelpText>Required</Form.HelpText>
+          </Form.Group>
+          <Form.Group controlId="deadline">
+            <Form.ControlLabel>Deadline Date</Form.ControlLabel>
+            <Form.Control name="deadline" accepter={DatePicker} oneTap block />
+          </Form.Group>
+          <Form.Group controlId="description">
+            <Form.ControlLabel>Description</Form.ControlLabel>
+            <Form.Control name="description" accepter={Textarea} />
+          </Form.Group>
+          <Form.Group>
+            <ButtonToolbar>
+              <Button appearance="primary" type="submit">
+                Submit
+              </Button>
+              <Button appearance="default" type="reset">
+                Cancel
+              </Button>
+            </ButtonToolbar>
+          </Form.Group>
+        </Form>
+      </Panel>
+    </>
   );
 }
