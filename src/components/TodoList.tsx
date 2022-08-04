002f4a1757cde0ac004fcedb618cab778ca1e7f9
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2022-08-04 09:26:23 +0700

    feat: adding animation while added todo

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,4 +1,4 @@
-import { Panel, List, FlexboxGrid, Col, ButtonGroup, Button } from "rsuite";
+import { Animation, Panel, List, FlexboxGrid, Col, Button } from "rsuite";
 import { observer } from "mobx-react-lite";
 import { todoStore } from "../store";
 import { TodoInterface } from "../types/Todo";
@@ -6,40 +6,42 @@ import TrashIcon from "@rsuite/icons/Trash";
 import CheckOutlineIcon from "@rsuite/icons/CheckOutline";
 
 const Item = ({ todo }: { todo: TodoInterface }) => (
-  <List.Item>
-    <FlexboxGrid justify="space-between">
-      <FlexboxGrid.Item
-        colspan={14}
-        className={`${todo.isDone && "line-through"}`}
-      >
-        <h4>{todo.title}</h4>
-        <small className="text-gray-400">{todo.deadline.toString()}</small>
-        <p>{todo.description}</p>
-        <p>{todo.isDone}</p>
-      </FlexboxGrid.Item>
-      <FlexboxGrid.Item as={Col} colspan={10} className="pt-8">
-        <div className="flex flex-row">
-          <Button
-            color="green"
-            appearance="primary"
-            onClick={() => todoStore.markAsDone(todo.id)}
-            className={`${todo.isDone && "invisible"} mr-2`}
-          >
-            <CheckOutlineIcon className="mr-2" />
-            Mark as Done
-          </Button>
-          <Button
-            color="red"
-            appearance="primary"
-            onClick={() => todoStore.removeTodo(todo.id)}
-          >
-            <TrashIcon className="mr-2" />
-            Remove
-          </Button>
-        </div>
-      </FlexboxGrid.Item>
-    </FlexboxGrid>
-  </List.Item>
+  <Animation.Collapse transitionAppear unmountOnExit in>
+    <List.Item>
+      <FlexboxGrid justify="space-between">
+        <FlexboxGrid.Item
+          colspan={14}
+          className={`${todo.isDone && "line-through"}`}
+        >
+          <h4>{todo.title}</h4>
+          <small className="text-gray-400">{todo.deadline.toString()}</small>
+          <p>{todo.description}</p>
+          <p>{todo.isDone}</p>
+        </FlexboxGrid.Item>
+        <FlexboxGrid.Item as={Col} colspan={10} className="pt-8">
+          <div className="flex flex-row">
+            <Button
+              color="green"
+              appearance="primary"
+              onClick={() => todoStore.markAsDone(todo.id)}
+              className={`${todo.isDone && "invisible"} mr-2`}
+            >
+              <CheckOutlineIcon className="mr-2" />
+              Mark as Done
+            </Button>
+            <Button
+              color="red"
+              appearance="primary"
+              onClick={() => todoStore.removeTodo(todo.id)}
+            >
+              <TrashIcon className="mr-2" />
+              Remove
+            </Button>
+          </div>
+        </FlexboxGrid.Item>
+      </FlexboxGrid>
+    </List.Item>
+  </Animation.Collapse>
 );
 
 const ItemObserver = observer(Item);
